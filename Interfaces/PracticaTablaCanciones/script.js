bdc627541749980b05738bd135ad5c3dd04f2df9
TIC <jorgerodriguezfolguera@gmail.com>
2023-10-04 17:51:11 +0200

    Tercera parte del formulario

--- Interfaces/PracticaTablaCanciones/script.js
+++ Interfaces/PracticaTablaCanciones/script.js
@@ -1,21 +1,10 @@
 function previewImage(event, querySelector){
 
-	//Recuperamos el input que desencadeno la acción
 	const input = event.target;
-	
-	//Recuperamos la etiqueta img donde cargaremos la imagen
 	$imgPreview = document.querySelector(querySelector);
-
-	// Verificamos si existe una imagen seleccionada
-	if(!input.files.length) return
-	
-	//Recuperamos el archivo subido
-	file = input.files[0];
-
-	//Creamos la url
-	objectURL = URL.createObjectURL(file);
-	
-	//Modificamos el atributo src de la etiqueta img
-	$imgPreview.src = objectURL;
-                
+	if(input.files.length){
+		file = input.files[0];
+		objectURL = URL.createObjectURL(file);
+		$imgPreview.src = objectURL;
+	}            
 }
