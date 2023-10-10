29feb908c1030ad3d0f59e6e705265eea82b3c0a
TIC <jorgerodriguezfolguera@gmail.com>
2023-10-10 17:05:38 +0200

    Ejercicio de mezclar colores sin resolver

--- D.Servidor/HojasDeEjercicios/EjercicioFichas/ejercicioficha.php
+++ D.Servidor/HojasDeEjercicios/EjercicioFichas/ejercicioficha.php
@@ -3,13 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>EJERCICIO DE LA FICHA</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        $posicion = rand(1,6);
-        echo("<img src='dado$posicion.jpeg' alt='$posicion'><br><br>");
-        echo("<img src='ficha$posicion.png' alt='$posicion'><br><br>");
-    ?>
+    <h1>EJERCICIO DE LA FICHA</h1>
+    <div>
+        <p>Actualiza la página para volver a tirar el dado y ver donde cae la ficha</p>
+        <?php
+            $posicion = rand(1,6);
+            echo("<img src='dado$posicion.jpeg' alt='$posicion'><br><br>");
+            echo("<img src='ficha$posicion.png' alt='$posicion'><br><br>");
+        ?>
+    </div>
 </body>
 </html>
--- D.Servidor/HojasDeEjercicios/EjercicioMezcla/ejercicioMezcla.php
+++ D.Servidor/HojasDeEjercicios/EjercicioMezcla/ejercicioMezcla.php
@@ -3,20 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Escaleras de X escalones</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <svg width='1000' height='1000'>
-        <?php
-            $numEscalones = rand(3,15);
-            $altura = 5;
-            $posicionX = 1;
-            for($i=0; $i<=$numEscalones; $i++){
-                echo("<rect x='$posicionX' width='40' height='$altura' style='fill:rgb(".rand(0,255).",".rand(0,255).",".rand(0,255).");stroke-width:1;stroke:rgb(0,0,0)' />");
-                $altura+=5;
-                $posicionX+=40;
-            }
-        ?>
-    </svg>
+    <h1>Escaleras de X escalones</h1>
+    <div>
+        <p>Recarga la pagina para cambiar la cantidad de escalones de la escalera y sus colores</p>
+        <svg width='5000' height='5000'>
+            <?php
+                $numEscalones = rand(10,40);
+                $altura = 5;
+                $posicionX = 1;
+                $posicionY = $numEscalones*5;
+                for($i=0; $i<=$numEscalones; $i++){
+                    echo("<rect x='$posicionX' y='$posicionY' width='40' height='$altura' style='fill:rgb(".rand(0,255).",".rand(0,255).",".rand(0,255).");stroke-width:1;stroke:rgb(0,0,0)' />");
+                    $altura+=5;
+                    $posicionX+=40;
+                    $posicionY-=5;
+                }
+            ?>
+        </svg>
+    </div>
 </body>
 </html>
