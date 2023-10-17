1e52e9379d95b69eabc5685587a24f641d3f7171
TIC <jorgerodriguezfolguera@gmail.com>
2023-10-17 20:51:04 +0200

    Pagina de camisetas provisional y duelo de dados 2 final

--- D.Servidor/JuegosDados/EjercicioDuelo2/duelo.php
+++ D.Servidor/JuegosDados/EjercicioDuelo2/duelo.php
@@ -40,11 +40,52 @@
                     $parejaJ1 = false;
                 }
 
+                $parejaJ2;
                 if($primerDadoJ2 == $segundoDadoJ2){
-                    $resultadoJ1 = "pareja";
+                    $parejaJ2 = true;
                 }else{
-                    $resultadoJ1 = "no pareja";
+                    $parejaJ2 = false;
                 }
+                echo($parejaJ1);
+
+                $resultado;
+                if($parejaJ1 == true && $parejaJ2 == false){
+                    $resultado = "Gana el jugador 1 por pareja de $primerDadoJ1";
+                }else if($parejaJ1 == false && $parejaJ2 == true){
+                    $resultado = "Gana el jugador 2 por pareja de $primerDadoJ2";
+                }else{
+                    $altoJ1;
+                    $bajoJ1;
+                    if($primerDadoJ1 > $segundoDadoJ1){
+                        $altoJ1 = $primerDadoJ1;
+                        $bajoJ1 = $segundoDadoJ1;
+                    }else{
+                        $altoJ1 = $segundoDadoJ1;
+                        $bajoJ1 = $primerDadoJ1;
+                    }
+
+                    $altoJ2;
+                    if($primerDadoJ2 > $segundoDadoJ2){
+                        $altoJ2 = $primerDadoJ2;
+                        $bajoJ2 = $segundoDadoJ2;
+                    }else{
+                        $altoJ2 = $segundoDadoJ2;
+                        $bajoJ2 = $primerDadoJ2;
+                    }
+
+                    if($altoJ1 > $altoJ2){
+                        $resultado = "Gana el jugador 1 por numero mas hato de $altoJ1";
+                    }else if($altoJ1 < $altoJ2){
+                        $resultado = "Gana el jugador 2 por numero mas hato de $altoJ2";
+                    }else if($bajoJ1 > $bajoJ2){
+                        $resultado = "Gana el jugador 1 por numero mas hato de $altoJ1";
+                    }else if($bajoJ1 < $bajoJ2){
+                        $resultado = "Gana el jugador 2 por numero mas hato de $altoJ2";
+                    }else{
+                        $resultado = "Empate";
+                    }
+                } 
+                echo("<p>$resultado</p>");
             ?>
         </div>
     </div>
