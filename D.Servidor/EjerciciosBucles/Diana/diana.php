09e747113f94ff88dc6eb73333b873ca38794fa2
TIC <jorgerodriguezfolguera@gmail.com>
2023-10-23 19:54:45 +0200

    Diana

--- D.Servidor/EjerciciosBucles/Diana/diana.php
+++ D.Servidor/EjerciciosBucles/Diana/diana.php
@@ -10,6 +10,7 @@
     <?php
     $tamano = 600;
     $numDisparos = rand(1,10);
+    echo("<svg width='1000' height='1000'>");
         for($indice = 1; $indice <= 10; $indice++){
             if($indice%2 == 0){
                 echo("<div style='background-color:white;border-radius:50%; zindex:$indice; height:".$tamano."px; width:".$tamano."px; padding:28px; text-align: center;'>");
@@ -23,6 +24,12 @@
             echo("<p'>$indice</p>") ;
             echo("</div>");
         }
+
+        for($indice = 0; $indice < $numDisparos; $indice++){
+            echo("<circle cx='".rand(28,600)."' cy='".rand(28,600)."' r='5' stroke='' fill='black' />") ;
+        }
+
+        echo("</svg>");
     ?>
 </body>
 </html>
